defb272e3eca58657ae395c92656cec3448cce31
7i10 <sy.9n3vesj@gmail.com>
2025-09-29 05:47:46 +0900

    generate_adversarial_samples関数にsteps引数を追加し、PGD攻撃の設定を改善

--- attacks/attack_generator.py
+++ attacks/attack_generator.py
@@ -2,14 +2,17 @@ import torchattacks
 
 
 def generate_adversarial_samples(
-    model, loader, attack_name="fgsm", epsilon=8 / 255, device="cuda"
+    model, loader, attack_name="fgsm", epsilon=8 / 255, device="cuda", steps=None
 ):
     model.to(device)
     model.eval()
     if attack_name == "fgsm":
         attack = torchattacks.FGSM(model, eps=epsilon)
     elif attack_name == "pgd":
-        attack = torchattacks.PGD(model, eps=epsilon)
+        if steps is not None:
+            attack = torchattacks.PGD(model, eps=epsilon, steps=steps)
+        else:
+            attack = torchattacks.PGD(model, eps=epsilon)
     else:
         raise ValueError("Unknown attack")
     # バッチ逐次処理型に変更
